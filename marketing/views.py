3a81ca3a705e9b9dea294b0f8e53395100f6fd03
Ranjani Krishnan <ranjanikrishnanr@gmail.com>
2020-05-16 00:23:08 +0200

    included logic for calculating cpi  and aggregating over cpi

--- marketing/views.py
+++ marketing/views.py
@@ -7,8 +7,7 @@ from django_filters.rest_framework import DjangoFilterBackend
 from django_filters import FilterSet
 from django_filters import rest_framework as filters
 from rest_framework.filters import OrderingFilter
-from django.db.models import Sum, Count
-from django.core import serializers
+from django.db.models import Sum, Count, F, FloatField, ExpressionWrapper
 from django.http import JsonResponse
 from .models import Metric
 
@@ -17,15 +16,18 @@ def performance(request):
     return HttpResponse("Hello, there")
 
 
+def calculate_cpi(queryset, fields):
+    field_set = list(set(fields) - set(['cpi']))
+    print('field_set: ', field_set)
+    qs_values = queryset.annotate(computed_cpi=ExpressionWrapper((F('spend') / F('installs')), output_field=FloatField())).values(*field_set).annotate(cpi=Sum('computed_cpi'))
+    return qs_values
+
 def sum_value(queryset, fields, sum_values):
     field_set = list(set(fields) - set(sum_values)) 
     qs_values = queryset.values(*field_set)
-
     for val in sum_values:
-        print('sum_values: ', val)
         sum_values_dict = { val: Sum(val) }
         qs_values = qs_values.annotate(**sum_values_dict)
-
     return qs_values
 
 
@@ -34,9 +36,10 @@ class MetricFilter(FilterSet):
     channel = filters.CharFilter('channel')
     date = filters.DateFromToRangeFilter()
     installs = filters.NumberFilter('installs')
+    cpi = filters.NumberFilter('cpi')
     class Meta:
         model = Metric
-        fields = ('date', 'channel', 'country', 'os', 'impressions', 'clicks', 'installs', 'spend', 'revenue',)
+        fields = ('date', 'channel', 'country', 'os', 'impressions', 'clicks', 'installs', 'spend', 'revenue','cpi')
 
 
 class MetricListView(generics.ListAPIView):
@@ -50,9 +53,12 @@ class MetricListView(generics.ListAPIView):
 
     def get_queryset(self):
         query_param = self.request.query_params
-        fields = query_param['fields'].split(',')
-        print('fields: ', fields)
-        sum_values = query_param['sum'].split(',')
-        aggr_sum = sum_value(self.queryset, fields, sum_values)
-        return aggr_sum
-        
+        if 'cpi_sum' in query_param:
+            fields = query_param['fields'].split(',')
+            return calculate_cpi(self.queryset, fields)
+        if 'sum' in query_param:
+            fields = query_param['fields'].split(',')
+            sum_values = query_param['sum'].split(',')
+            aggr_sum = sum_value(self.queryset, fields, sum_values)
+            return aggr_sum
+        return self.queryset
