ea6e2c4d9d8639def5b5a1bc0b45d6d4948ee51e
Ranjani Krishnan <ranjanikrishnanr@gmail.com>
2020-05-16 00:23:39 +0200

    Added cpi in serializer

--- marketing/serializers.py
+++ marketing/serializers.py
@@ -1,9 +1,13 @@
-from rest_framework.serializers import ModelSerializer, IntegerField
+from rest_framework.serializers import ModelSerializer, SerializerMethodField, ReadOnlyField
 from drf_queryfields import QueryFieldsMixin
 from .models import Metric
 
 
 class MetricSerializer(QueryFieldsMixin, ModelSerializer):
+
+
     class Meta:
         model = Metric
-        fields = ['date','channel','country','os','impressions','clicks','installs','spend','revenue']
+        fields = ['date','channel','country','os','impressions','clicks','installs','spend','revenue','cpi']
+
+
